bc1e2845065b83d39b0916b8ac96bdf0e6819d28
Ethan Chaplin Lee <chaplinleee@gmail.com>
2024-12-28 14:53:59 -0500

    v:1.6

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,11 +14,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Facivons and Thumbnails for Google and Safari */}
         <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png" />
         <link rel="manifest" href="favicon_io/site.webmanifest" />
+        
+        {/* Instantly Pixel Code Script for Website Data Tracking */}
         <script id="vtag-ai-js" async src="https://r2.leadsy.ai/tag.js" data-pid="1gNebR82irMIRhJ10" data-version="062024"></script>
+      
       </head>
       <body className="bg-black">
         {children}
